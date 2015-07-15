91527e04200dae730d1ba4e4bca1b71c1ce36fbe
rememberlenny <rememberlenny@gmail.com>
2015-07-14 22:00:32 -0400

    Save the trending extension

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,84 @@
 var colors = ['rgb(15, 157, 88)', 'rgb(219, 68, 55)', 'rgb(66, 133, 244)', 'rgb(244, 180, 0)'];
 var index = 0;
+
+
 $(document).ready(function(){
-  $('.recommended-stories li').on('click', function(e){
-    index = index + 1;
-    if( index == 4){index = 0}
-    e.preventDefault();
-    var url = $(this).children().data('url');
-    $('.grid-frames').css('background', colors[index]);
-    $('.grid-frames').html(url);
-  });
+  load_trending_topics();
 });
+
+function first(obj) {
+  for (var a in obj) return a;
+}
+
+function load_trending_topics(){
+  var url = 'http://edna-trending.herokuapp.com/trending/';
+  var $story_container = $('.recommended-stories');
+  $story_container.html('');
+
+  $.ajax({
+    url: url,
+  }).done(function(datas) {
+    var data = datas['1'];
+    for ( i = 0; i < 19; i++ ){
+      var story_el = generate_trends_template(data, i);
+      $story_container.append(story_el);
+    }
+    $('.recommended-stories li').on('click', function(e){
+      clicked_recommended(e, this);
+    });
+  });
+}
+
+function clicked_recommended(e, self){
+  e.preventDefault();
+  index = index + 1;
+  index == 4 ? index = 0 : index;
+
+  var keyword = $(self).data('keyword');
+  var url = 'http://edna-trending.herokuapp.com/parsely/' + encodeURIComponent(keyword);
+  var datas = [];
+
+
+  $('.grid-frames *').fadeOut();
+  $.ajax({
+    url: url,
+  }).done(function(datas) {
+    keyword = keyword.split(' ');
+    keyword = keyword.join('+');
+    do_on_parsely_return(datas, keyword)
+  });
+}
+
+function generate_trends_template(datas, elN){
+  var template = '<li data-keyword="' + datas[elN] + '"><a href="#"><span class="block-list-label">' + elN + '</span></strong>' + datas[elN] + '</a></li>';
+  return template;
+}
+
+function generate_recirc_template(response, elN){
+  var datas = response['response']['data'];
+  console.log('datas', datas[elN]);
+  var image = datas[elN]['image_url'];
+  var postDate = datas[elN]['pub_date'];
+  postDate = postDate.split('T');
+  if (image == ''){ image = 'http://www.newyorker.com/wp-content/assets/dist/img/cartoon-404.png' }
+  var template = '<div class="grid-content"><div class=" card"><a href="' + datas[elN]['url'] + '"><img src="' + image + '"><div class="card-section"><h5 class="subtitle">' + postDate + '</h5><h4>' + datas[elN]['title'] + '</h4></div></a></div></div>';
+  return template;
+}
+
+
+
+function do_on_parsely_return(datas, keyword){
+  recirculation = '<div class="grid-block ng-scope"><h1>' + keyword + '</h1></div>';
+  recirculation = recirculation + '<div class="grid-block small-up-1 medium-up-3 ng-scope text-center"><iframe style="  margin: 0 auto; background: white; height: 370px; width: 80%" src="http://www.google.com/trends/fetchComponent?q=' + keyword + '&cid=TIMESERIES_GRAPH_0&export=5"></iframe></div>';
+  recirculation = recirculation + '<div class="grid-block ng-scope"><h3>EDNA Recommends</h3></div>';
+  recirculation = recirculation + '<div class="grid-block small-up-1 medium-up-3 ng-scope">';
+  for(i = 0; i < 3; i++){
+    recirculation = recirculation + generate_recirc_template(datas, i);
+  }
+  recirculation = recirculation + '</div>';
+
+  template = recirculation;
+
+  $('.grid-frames').css('background', colors[index]);
+  $('.grid-frames').html(template);
+}
